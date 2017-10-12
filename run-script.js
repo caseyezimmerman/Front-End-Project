8d4f5895d2be4e038168fa8fc810a9a0ad6ef71c
chris-malloy <chrismalloymusic@gmail.com>
2017-10-12 15:59:06 -0400

    added api key

--- run-script.js
+++ run-script.js
@@ -1,8 +1,8 @@
 console.log("test")
 
-$(document).ready(function(){
-	setTimeout(function(){
-	$('#myModal').modal('show')
-}, 2000)
-
+$(document).ready(function() {
+    setTimeout(function() {
+        $('#myModal').modal('show')
+    }, 2000)
+    var url = "https://maps.googleapis.com/maps/api/js?key=apiKey&callback=initMap"
 })
