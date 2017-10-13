14365df61a140b8c36be12b925d102547b3d5351
chris-malloy <chrismalloymusic@gmail.com>
2017-10-13 15:33:14 -0400

    added map, need to debug

--- run-script.js
+++ run-script.js
@@ -4,5 +4,20 @@ $(document).ready(function() {
     setTimeout(function() {
         $('#myModal').modal('show')
     }, 2000)
-    var url = "https://maps.googleapis.com/maps/api/js?key=apiKey&callback=initMap"
+
+    function initMap() {
+        var uluru = { lat: -25.363, lng: 131.044 };
+        var map = new google.maps.Map($('#map'), {
+            zoom: 4,
+            center: uluru
+        });
+        console.log(map)
+        var marker = new google.maps.Marker({
+            position: uluru,
+            map: map
+        });
+    }
+    var googleMapsScript = document.createElement('script');
+    googleMapsScript.setAttribute('src', `https://maps.googleapis.com/maps/api/js?key=${apiKey}&callback=initMap`);
+    document.head.appendChild(googleMapsScript);
 })
