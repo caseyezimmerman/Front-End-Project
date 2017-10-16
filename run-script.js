1f7f8af7e747eb4297f6598b94275955e070a2d1
chris-malloy <chrismalloymusic@gmail.com>
2017-10-16 08:17:09 -0400

    switched function to native js

--- run-script.js
+++ run-script.js
@@ -15,8 +15,11 @@ $(document).ready(function() {
     }, 2000)
 
     function initMap() {
-        var uluru = { lat: -25.363, lng: 131.044 };
-        var map = new google.maps.Map($('#map'), {
+        var uluru = {
+            lat: -25.363,
+            lng: 131.044
+        };
+        var map = new google.maps.Map(document.getElementById('map'), {
             zoom: 4,
             center: uluru
         });
@@ -26,6 +29,7 @@ $(document).ready(function() {
             map: map
         });
     }
+
     var googleMapsScript = document.head.children[1];
     googleMapsScript.setAttribute('src', `https://maps.googleapis.com/maps/api/js?key=${apiKey}&callback=initMap`);
 
